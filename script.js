a10067851d613612b541af3721f340502a868ab7
Fede <fedelc.us@gmail.com>
2014-09-24 22:30:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+//<![CDATA[ 
+window.addEvent('load', function() {
+var f = document.getElementById('foo');
+document.addEventListener('click', function(ev){
+    f.style.left = (ev.clientX-25)+'px';
+    f.style.top = (ev.clientY-25)+'px';
+},false);    
+
+});//]]>  
+   
+
+	function drag(ev) {
+	    ev.dataTransfer.setData("text", ev.target.id);
+	}
+	function allowDrop(ev) {
+	    ev.preventDefault();
+	}
+	function drop(ev) {
+	    ev.preventDefault();
+	    var data = ev.dataTransfer.getData("text");
+	    ev.target.appendChild(document.getElementById(data));
+	}
