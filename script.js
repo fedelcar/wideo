bc17c431c3b21ead52dbe8d03428d12bb7b8ddbe
Fede <fedelc.us@gmail.com>
2014-09-28 12:54:04 -0300

    Controls for x,y,alpha,rotation,transition style.

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 
 
 window.addEvent('load', function() {
-var f = document.getElementById('foo');
-document.addEventListener('click', function(ev){
-    f.style.left = (ev.clientX-25)+'px';
-    f.style.top = (ev.clientY-25)+'px';
-},false);    
+	var f = document.getElementById('foo');
+	document.addEventListener('click', function(ev){
+	   // f.style.left = (ev.clientX-25)+'px';
+	   // f.style.top = (ev.clientY-25)+'px';
+	   // f.style.width = '70px';
+	   // f.style.height = '70px';
+	},false);    
 
+
+	document.getElementById("playButton").onclick = function () { 
+		//alert('scale(1.5) ' + 'rotate(' + document.getElementById('rotation').value + 'deg)');
+
+		var f = document.getElementById('foo');
+		
+		
+		var transition_type = 'linear';
+		if (document.getElementById('type1').checked) {
+  			transition_type = document.getElementById('type1').value;
+		}
+		if (document.getElementById('type2').checked) {
+  			transition_type = document.getElementById('type2').value;
+		}
+		f.style.transition =  'all ' +document.getElementById('time').value+'s '+ transition_type;
+		f.style.left = document.getElementById('x').value + 'px';
+		f.style.top = document.getElementById('y').value + 'px'; 
+		f.style.transform = 'rotate(' + document.getElementById('rotation').value + 'deg)';
+		
+
+		f.style.opacity = document.getElementById('alpha').value; 
+	};
 });
+
+
    
+
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
