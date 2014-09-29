589633aa17e580a87da2cac808f3a3b28d5992fa
Fede <fedelc.us@gmail.com>
2014-09-29 00:01:24 -0300

    Play, reset, marker moving.

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
+function playButton() { 
+	var f = document.getElementById('box');
+	var marker = document.getElementById('marker');
+	var timeline = document.getElementById('timeline');
+	
 
+	timeline.style.width='100%';
+	//alert(timeline.style.width+'px');
+	
+	var transition_type = 'linear';
+	if (document.getElementById('type1').checked) {
+			transition_type = document.getElementById('type1').value;
+	}
+	if (document.getElementById('type2').checked) {
+			transition_type = document.getElementById('type2').value;
+	}
 
-window.addEvent('load', function() {
-	var f = document.getElementById('foo');
-	document.addEventListener('click', function(ev){
-	   // f.style.left = (ev.clientX-25)+'px';
-	   // f.style.top = (ev.clientY-25)+'px';
-	   // f.style.width = '70px';
-	   // f.style.height = '70px';
-	},false);    
+	f.style.transition =  'all ' +document.getElementById('time').value+'s '+ transition_type;
+	f.style.left = document.getElementById('x').value + 'px';
+	f.style.top = document.getElementById('y').value + 'px'; 
+	f.style.transform = 'rotate(' + document.getElementById('rotation').value + 'deg)';
+	f.style.opacity = document.getElementById('alpha').value; 
 
-
-	document.getElementById("playButton").onclick = function () { 
-		//alert('scale(1.5) ' + 'rotate(' + document.getElementById('rotation').value + 'deg)');
-
-		var f = document.getElementById('foo');
-		
-		
-		var transition_type = 'linear';
-		if (document.getElementById('type1').checked) {
-  			transition_type = document.getElementById('type1').value;
-		}
-		if (document.getElementById('type2').checked) {
-  			transition_type = document.getElementById('type2').value;
-		}
-		f.style.transition =  'all ' +document.getElementById('time').value+'s '+ transition_type;
-		f.style.left = document.getElementById('x').value + 'px';
-		f.style.top = document.getElementById('y').value + 'px'; 
-		f.style.transform = 'rotate(' + document.getElementById('rotation').value + 'deg)';
-		
-
-		f.style.opacity = document.getElementById('alpha').value; 
+	marker.style.transition =  'all ' +document.getElementById('time').value+'s linear';
+	marker.style.left = timeline.style.width;
 	};
-});
 
 
-   
+function resetButton() { 
+	var marker = document.getElementById('marker');
+	var f = document.getElementById('box');
+
+	f.style.transform = 'rotate(0deg)';
+	f.style.transition =  'all 0s linear';  
+	marker.style.transition = 'all 0s linear';  
+	marker.style.left = 0;
+	
+	document.getElementById('x').value=0;
+	document.getElementById('y').value=0;
+	document.getElementById('alpha').value=1;
+	document.getElementById('rotation').value=0;
+	document.getElementById('type1').checked=true;
+
+	f.style.left = '0px';
+	f.style.top = '0px';
+	};
+
+window.onload = function() {
+  playButton();
+  resetButton();
+};
 
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
