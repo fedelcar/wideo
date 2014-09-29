702da80b2d951d426e8ae75480e50f6ade349a63
fedelcar <fedelc.us@gmail.com>
2014-09-29 12:50:49 -0300

    Final functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+  playButton();
+  resetButton();
+};
+
 function playButton() { 
 	var f = document.getElementById('box');
 	var marker = document.getElementById('marker');
@@ -5,7 +10,7 @@ function playButton() {
 	
 
 	timeline.style.width='100%';
-	//alert(timeline.style.width+'px');
+	
 	
 	var transition_type = 'linear';
 	if (document.getElementById('type1').checked) {
@@ -14,11 +19,20 @@ function playButton() {
 	if (document.getElementById('type2').checked) {
 			transition_type = document.getElementById('type2').value;
 	}
+	if (document.getElementById('type3').checked) {
+			transition_type = document.getElementById('type3').value;
+	}
+	if (document.getElementById('type4').checked) {
+			transition_type = document.getElementById('type4').value;
+	}
+	if (document.getElementById('type5').checked) {
+			transition_type = document.getElementById('type5').value;
+	}
 
 	f.style.transition =  'all ' +document.getElementById('time').value+'s '+ transition_type;
 	f.style.left = document.getElementById('x').value + 'px';
 	f.style.top = document.getElementById('y').value + 'px'; 
-	f.style.transform = 'rotate(' + document.getElementById('rotation').value + 'deg)';
+	f.style.transform = 'rotate(' + document.getElementById('rotation').value + 'deg) ' + 'scale(' + document.getElementById('size').value + ')';
 	f.style.opacity = document.getElementById('alpha').value; 
 
 	marker.style.transition =  'all ' +document.getElementById('time').value+'s linear';
@@ -30,7 +44,7 @@ function resetButton() {
 	var marker = document.getElementById('marker');
 	var f = document.getElementById('box');
 
-	f.style.transform = 'rotate(0deg)';
+	f.style.transform = 'rotate(0deg) scale(1)';
 	f.style.transition =  'all 0s linear';  
 	marker.style.transition = 'all 0s linear';  
 	marker.style.left = 0;
@@ -38,16 +52,39 @@ function resetButton() {
 	document.getElementById('x').value=0;
 	document.getElementById('y').value=0;
 	document.getElementById('alpha').value=1;
+	document.getElementById('size').value=1;
 	document.getElementById('rotation').value=0;
 	document.getElementById('type1').checked=true;
 
 	f.style.left = '0px';
 	f.style.top = '0px';
+	f.style.opacity = 1;
+
 	};
 
-window.onload = function() {
-  playButton();
-  resetButton();
-};
+function clickTimeline(event) { 
+	var f = document.getElementById('box');
+	var marker = document.getElementById('marker');
+	var timeline = document.getElementById('timeline');
 
+	var xIni = f.style.left;
+	var yIni = f.style.top;
+	var alphaIni = 1;
+	var sizeIni = 1;
+	
+	var markerpos = (event.x-timeline.getBoundingClientRect().left);
+	var time = markerpos / timeline.getBoundingClientRect().width;
+	//alert(alphaIni);
+
+	f.style.transition =  'all 0s linear';
+	f.style.left = ((document.getElementById('x').value)*time )+ 'px';
+	f.style.top = ((document.getElementById('y').value) *time)+ 'px'; 
+	f.style.transform = 'rotate(' + (document.getElementById('rotation').value*time) + 'deg) ' + 'scale(' + ((document.getElementById('size').value-1)*time+1) + ')';
+	f.style.opacity = 1-(1-document.getElementById('alpha').value) * time; 
+
+	marker.style.transition = 'all 0s linear';  
+	marker.style.left =markerpos+'px';
+
+
+	};
 
